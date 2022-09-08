c1ffce4920b458683e589f7bc87ae67d72a22f17
mektigboy <mektigboy@protonmail.com>
2022-09-08 00:09:39 -0500

    Update contracts/CampaignFactory.sol

--- contracts/CampaignFactory.sol
+++ contracts/CampaignFactory.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.8.15;
 
 import "./Campaign.sol";
 
-contract CampaignFactory {}
+error CampaignFactory__NotClient();
+
+contract CampaignFactory {
+    uint256 campaignId;
+    Campaign[] public campaignList;
+
+    // MODIFIERS
+
+    // modifier onlyClient() {}
+
+    constructor() {}
+
+    function createCampaign() public /* onlyClient */
+    {
+        Campaign newCampaign = new Campaign();
+        campaignList.push(newCampaign);
+    }
+}
