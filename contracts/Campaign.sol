661c23ddcf22187d9537a5e361aac0fb97da0281
mektigboy <mektigboy@protonmail.com>
2022-09-08 00:09:19 -0500

    Update contracts/Campaign.sol

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,11 +2,5 @@
 pragma solidity ^0.8.15;
 
 contract Campaign {
-    struct NewCampaign {
-        uint256 campaignId;
-        string campaignName;
-    }
-
-    // mapping(address => Advertisment) private campaigns;
-    mapping(address => mapping(uint256 => NewCampaign)) private campaigns;
+    constructor() {}
 }
