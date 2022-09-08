3c3127455fbc003b98e8a3dfe08b96c3e85e2eee
mektigboy <mektigboy@protonmail.com>
2022-09-08 00:10:06 -0500

    Update contracts/PublisherRegistration.sol

---
+++ contracts/PublisherRegistration.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/// @title Publisher Registration
+/// @author
+/// @notice Registers publishers (user who wants to display ads on their website).
+/// @dev
+contract PublisherRegistration {
+
+}
