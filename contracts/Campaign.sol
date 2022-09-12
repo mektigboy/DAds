d0d5c25ff5d3d45b6133d3fe04b3e7949540f93f
mektigboy <mektigboy@protonmail.com>
2022-09-11 23:18:45 -0500

    Update contracts/Campaign.sol

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+error Campaign__NotCampaignManager();
+
 contract Campaign {
-    constructor() {}
+    address private s_campaignManager;
+    uint256 private s_campaignBudget;
+
+    modifier onlyCampaignManager() {
+        if (s_campaignManager != msg.sender)
+            revert Campaign__NotCampaignManager();
+        _;
+    }
+
+    constructor() payable {
+        s_campaignManager = msg.sender;
+        s_campaignBudget = msg.value;
+    }
 }
