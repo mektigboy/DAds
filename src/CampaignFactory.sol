6375831cffa1d739eb0237845e6a110afe1d681f
mektigboy <mektigboy@protonmail.com>
2022-09-23 13:30:08 -0500

    Update src/CampaignFactory.sol

--- src/CampaignFactory.sol
+++ src/CampaignFactory.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.17;
 
 import "./Campaign.sol";
 
+/// @title CampaignFactory
+/// @author mektigboy
+/// @notice
+/// @dev
 contract CampaignFactory {
-    Campaign newCampaign;
+    ///////////////
+    /// STORAGE ///
+    ///////////////
 
     address private s_owner;
     uint256 private s_campaignCounter;
@@ -15,16 +21,24 @@ contract CampaignFactory {
     /// @dev Campaign Id => Campaign Owner
     mapping(uint256 => address) private s_ownedCampaigns;
 
+    ///////////////////
+    /// CONSTRUCTOR ///
+    ///////////////////
+
     constructor() {
         s_owner = msg.sender;
     }
+
+    /////////////////
+    /// MODIFIERS ///
+    /////////////////
 
     modifier onlyRegistredUsers() {
         _;
     }
 
     function createCampaign(uint256 budget) public {
-        newCampaign = new Campaign(budget, msg.sender);
+        Campaign newCampaign = new Campaign(budget, msg.sender);
         uint256 campaignId = s_campaignCounter + 1;
 
         s_deployedCampaigns[campaignId] = address(newCampaign);
