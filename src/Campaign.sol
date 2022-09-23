ceb66ec07df4db5f564aad4c3a1eff03cf4f3a1c
mektigboy <mektigboy@protonmail.com>
2022-09-23 13:29:55 -0500

    Update src/Campaign.sol

--- src/Campaign.sol
+++ src/Campaign.sol
@@ -1,17 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/// @title Campaign
+/// @author mektigboy
+/// @notice
+/// @dev
 contract Campaign {
-    uint256 private s_campaignBudget;
-    address private s_campaignOwner;
+    ////////////////
+    /// CAMPAIGN ///
+    ////////////////
+
+    /// Structure:
+    /// 1) Image
+    /// 2) Name
+    /// 3) Website
+    /// 4) Keywords (Min. 3)
+    /// 5) Demography
+    /// 6) Budget
+    /// 7) Duration
+
+    ////////////////////
+    /// ADVERTISMENT ///
+    ////////////////////
 
     struct Advertisment {
+        string image;
         string name;
-        string description;
+        string website;
+        string[3] keywords;
+        string demography;
         uint256 budget;
-        address owner;
-        bool isActive;
+        uint256 duration;
+        address recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
     }
+
+    ///////////////
+    /// STORAGE ///
+    ///////////////
+
+    uint256 private s_campaignBudget;
+    address private s_campaignOwner;
+    address private s_campignManager;
+
+    ///////////////////
+    /// CONSTRUCTOR ///
+    ///////////////////
 
     constructor(uint256 budget, address owner) {
         s_campaignBudget = budget;
