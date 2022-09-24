b845c49ecd85d71038e7302cb64355fe8fbf548d
Franco De Leon <francodeleon97@hotmail.com>
2022-09-23 22:12:16 -0500

    remove comment

--- ui/components/AppHeader.js
+++ ui/components/AppHeader.js
@@ -4,19 +4,6 @@ import { ethers } from "ethers";
 export default function AppHeader() {
   const [currentWalletAddress, setCurrentWalletAddress] = useState("");
 
-  //   async function connectWallet() {
-  //     if (window.ethereum) {
-  //       await window.ethereum.request({ method: "eth_requestAccounts" });
-  //       window.web3 = new Web3(window.ethereum);
-  //       const account = window.web3.eth.accounts;
-  //       const walletAddress = account.givenProvider.selectedAddress;
-
-  //       setCurrentWalletAddress(walletAddress);
-  //     } else {
-  //       console.log("No wallet");
-  //     }
-  //   }
-
   async function connectWallet() {
     if (!currentWalletAddress) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
