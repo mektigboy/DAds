be23ceb789253f5273b94cf0a596aabf0e13bd4b
mektigboy <mektigboy@protonmail.com>
2022-09-23 20:50:33 -0500

    Update src/Campaign.sol

--- src/Campaign.sol
+++ src/Campaign.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+error Campaign__OnlyCampaignManager();
+
 /// @title Campaign
 /// @author mektigboy
 /// @notice
 /// @dev
 contract Campaign {
+    ///////////////
+    /// STORAGE ///
+    ///////////////
+
     ////////////////
     /// CAMPAIGN ///
     ////////////////
@@ -19,38 +25,84 @@ contract Campaign {
     /// 6) Budget
     /// 7) Duration
 
-    ////////////////////
-    /// ADVERTISMENT ///
-    ////////////////////
+    ///////////////////////
+    /// CAMPAIGN INPUTS ///
+    ///////////////////////
 
-    struct Advertisment {
-        string image;
-        string name;
-        string website;
-        string[3] keywords;
-        string demography;
-        uint256 budget;
-        uint256 duration;
-        address recipient;
-        bool complete;
-        uint256 approvalCount;
-        mapping(address => bool) approvals;
-    }
+    string private s_image;
+    string private s_name;
+    string private s_website;
+    string[10] private s_keywords;
+    string private s_demography;
+    uint256 private s_budget;
+    uint256 private s_duration;
 
-    ///////////////
-    /// STORAGE ///
-    ///////////////
+    /////////////////////
+    /// CAMPAIGN DATA ///
+    /////////////////////
 
-    uint256 private s_campaignBudget;
-    address private s_campaignOwner;
-    address private s_campignManager;
+    address private s_manager;
+    uint256 private s_creationDate;
+    uint256 private s_expirationDate;
+    bool private s_isCampaignActive;
+    uint256 private s_approvalCount;
+
+    mapping(address => bool) s_approvals;
 
     ///////////////////
     /// CONSTRUCTOR ///
     ///////////////////
 
-    constructor(uint256 budget, address owner) {
-        s_campaignBudget = budget;
-        s_campaignOwner = owner;
+    constructor(
+        string memory image,
+        string memory name,
+        string memory website,
+        string[3] memory keywords,
+        string memory demography,
+        uint256 duration /* days */
+    ) payable {
+        s_image = image;
+        s_name = name;
+        s_website = website;
+        s_keywords = keywords;
+        s_demography = demography;
+        s_budget = msg.value; // ETH
+        s_duration = duration * 86400; // 86400 = 1 day
+
+        s_manager = msg.sender;
+        s_creationDate = block.timestamp;
+        
+        s_expirationDate = s_creationDate + s_duration;
+        s_isCampaignActive = true;
+    }
+
+    /////////////////
+    /// MODIFIERS ///
+    /////////////////
+
+    modifier onlyCampaignManager() {
+        if (s_manager != msg.sender)
+            revert Campaign__OnlyCampaignManager();
+        _;
+    }
+
+    function updateCampaignManager(address newManager) public onlyCampaignManager {
+        s_manager = newManager;
+    }
+
+    ///////////////
+    /// GETTERS ///
+    ///////////////
+
+    function getCampaignImage() public view returns (string memory) {
+        return s_image;
+    }
+
+    function getCampaignWebsite() public view returns (string memory) {
+        return s_website;
+    }
+
+    function getCampaignManager() public view returns (address) {
+        return s_manager;
     }
 }
