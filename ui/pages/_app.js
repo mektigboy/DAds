381d4873491edd21a183dbd7dd1fd6de67e3d004
Franco De Leon <francodeleon97@hotmail.com>
2022-09-24 18:19:37 -0500

    remove comments from app

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,34 +1,5 @@
 import "../styles/globals.css";
 import { create as ipfsCreate } from "ipfs";
-import OrbitDB from "orbit-db";
-import { useEffect } from "react";
-
-// let ipfs;
-// let orbitdb;
-// let db;
-
-// (async function () {
-//   ipfs = await ipfsCreate();
-//   orbitdb = await OrbitDB.createInstance(ipfs);
-
-//   // Create / Open a database
-//   db = await orbitdb.log("hello");
-//   await db.load();
-
-//   // Listen for updates from peers
-//   db.events.on("replicated", (address) => {
-//     console.log(db.iterator({ limit: -1 }).collect());
-//   });
-
-//   // Add an entry
-//   const hash = await db.add("world");
-//   console.log(hash);
-
-//   // Query
-//   const result = db.iterator({ limit: -1 }).collect();
-//   console.log(JSON.stringify(result, null, 2));
-// })();
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
